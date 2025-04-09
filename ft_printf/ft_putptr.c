18531766d431951747c6c8e5b4f016af494d3f9c
OnurOksuz59005 <onur.vizja@gmail.com>
2025-04-09 18:01:34 +0200

    correction for ft_printf

--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -6,6 +6,8 @@ int	ft_putptr(void *ptr)
 	unsigned long	addr;
 
 	count = 0;
+	if (ptr == NULL)
+		return (ft_putstr("(nil)"));
 	addr = (unsigned long)ptr;
 	count += ft_putstr("0x");
 	count += ft_puthex(addr, 0);
